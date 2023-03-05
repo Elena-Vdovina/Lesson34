e327edcd5fbfe4c88aa79c28bec9fd4120772a13
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-05 14:10:16 +0100

    Homework2 keybord input

--- src/Homework2.java
+++ src/Homework2.java
@@ -1,5 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Homework2 {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
 // В файловую систему одного суперкомпьютера проник вирус, который сломал контроль за правами
 // доступа к файлам. Для каждого файла известно, с какими действиями можно к нему обращаться:
 //    запись W,
@@ -17,5 +23,30 @@ public class Homework2 {
 // Файл results.txt
 // Ваша программа для каждого запроса должна будет выводить Файл: Операция: OK, если над файлом
 // выполняется допустимая операция, или же Файл: Операция: Access denied, если операция недопустима.
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    Map<String, String> filesOperator = new HashMap<>();
+    for (int i = 0; i < n; ++i) {
+      String line = br.readLine();
+      int dot = line.indexOf(' ');
+      String files = line.toLowerCase().substring(0, dot);
+      String operat = line.toLowerCase().substring(dot + 1);
+      filesOperator.put(files, operat);
+    }
+    int m = Integer.parseInt(br.readLine());
+    for (int i = 0; i < m; ++i) {
+      String line = br.readLine();
+      int dot = line.indexOf(' ');
+      String file = line.toLowerCase().substring(dot + 1);
+      String operation = line.toLowerCase().substring(0, dot);
+      String operN = operation.substring(0, 1);
+      if (filesOperator.containsKey(file)) {
+        if (filesOperator.get(file).contains(operN)) {
+          System.out.println(file + ": " + operation + ": OK");
+        } else {
+          System.out.println(file + ": " + operation + ": Access denied");
+        }
+      }
+    }
   }
 }
