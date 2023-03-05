fce44d773edc2f678bff309ff9aed405bf4d8e60
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-05 11:24:45 +0100

    Homework1 - keyboard input

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,5 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Homework1 {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
 // Напишите программу создания небольшого словаря сленговых программерских выражений,
 // чтобы она потом по запросу возвращала значения из этого словаря.
 // Формат входных данных
@@ -13,5 +19,30 @@ public class Homework1 {
 //  Для каждого слова, независимо от регистра символов, если оно присутствует в словаре,
 //  необходимо вывести на экран его определение.
 // Если слова в словаре нет, программа должна вывести "Не найдено", без кавычек.
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    Map<String, String> dictionary = new HashMap<>(n);
+    for (int i = 0; i < n; ++i) {
+      String line = br.readLine();
+      int dot=line.indexOf(':');
+      String word = line.toLowerCase().substring(0,dot);
+      String definition = line.substring(dot+2);
+      dictionary.put(word, definition);
+    }
+
+    int m = Integer.parseInt(br.readLine());
+    for (int i = 0; i < m; ++i) {
+      String line = br.readLine();
+      String word = line.toLowerCase();
+      if(dictionary.containsKey(word)){
+        String definition = dictionary.get(word);
+        System.out.println(line + ": " + definition);
+      } else{
+        System.out.println("Не найдено");
+      }
+
+    }
   }
+
+
 }
